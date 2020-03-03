8f5ec8d963ea253d9b10051e67122bbea2ccb449
ElfimovArtem <elfimov021@yandex.ru>
2020-03-03 13:12:42 +0300

    feat: decided task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    for (let i = 0; i < bracketsConfig.length;) {
+        let brackets = bracketsConfig[i].join('');
+
+        if (str.includes(brackets)) {
+            str = str.replace(brackets, '');
+            i = 0;
+        } else {
+            i++;
+        }
+    }
+
+    let test;
+    if (str.length > 0) {
+        test = false;
+    } else {
+        test = true;
+    }
+    return test;
+};
